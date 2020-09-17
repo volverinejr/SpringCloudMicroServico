41c8bceb2ea80b3e8ac7dacc2b393770bb762d8a
volverinejr <volverinejr@gmail.com>
2020-09-17 14:31:38 -0300

    Criando o Microservico EurekaServer

---
+++ Backend/servico-eureka-server/src/main/java/com/claudemirojr/app/eureka/ServicoEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.claudemirojr.app.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServicoEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoEurekaServerApplication.class, args);
+	}
+
+}
---
+++ Backend/servico-eureka-server/src/test/java/com/claudemirojr/app/eureka/ServicoEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claudemirojr.app.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
