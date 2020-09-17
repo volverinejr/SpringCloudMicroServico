2bf34c477d5f84ca2c34355e21593d84dd60f830
volverinejr <volverinejr@gmail.com>
2020-09-17 14:02:41 -0300

    Balanceando a carga com Ribbon

--- Backend/servico-items/src/main/java/com/claudemirojr/app/items/clientes/ProdutoClienteRest.java
+++ Backend/servico-items/src/main/java/com/claudemirojr/app/items/clientes/ProdutoClienteRest.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.claudemirojr.app.items.models.Produto;
 
-@FeignClient(name = "servico-produtos", url = "localhost:8001")
+@FeignClient(name = "servico-produtos")
 public interface ProdutoClienteRest {
 
 	@GetMapping("/listar")
--- Backend/servico-produtos/src/main/java/com/claudemirojr/app/produtos/controller/ProdutoController.java
+++ Backend/servico-produtos/src/main/java/com/claudemirojr/app/produtos/controller/ProdutoController.java
@@ -3,6 +3,7 @@ package com.claudemirojr.app.produtos.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,16 +14,23 @@ import com.claudemirojr.app.produtos.models.service.IProdutoService;
 @RestController
 public class ProdutoController {
 
+	@Value("${server.port}")
+	private Integer port;
+
 	@Autowired
 	private IProdutoService produtoService;
 
 	@GetMapping("/listar")
 	public List<Produto> listar() {
+		System.out.println("PORTA: " + port);
+
 		return produtoService.findAll();
 	}
 
 	@GetMapping("/ver/{id}")
 	public Produto detalhe(@PathVariable Long id) {
+		System.out.println("PORTA: " + port);
+
 		return produtoService.findById(id);
 	}
 
